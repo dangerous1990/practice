b60b24328abadc5f71bfe9c0ee910b31ba6531d4
dangerous1990 <dangerouslm1990@gmail.com>
2016-11-01 11:19:46 +0800

    https://leetcode.com/problems/unique-paths-ii/

---
+++ src/main/java/org/learn/leetcode/dp/UniquePaths.java
@@ -0,0 +1,33 @@
+package org.learn.leetcode.dp;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2016/11/1.
+ */
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if (i == 0) {
+                    dp[i][j] = dp[i][j - 1];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    @Test
+    public void test() {
+        System.out.println(uniquePaths(2, 2));
+    }
+}
---
+++ src/main/java/org/learn/leetcode/dp/UniquePathsII.java
@@ -0,0 +1,42 @@
+package org.learn.leetcode.dp;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2016/11/1.
+ */
+public class UniquePathsII {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    if (i == 0) {
+                        dp[i][j] = dp[i][j - 1];
+                    } else if (j == 0) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
